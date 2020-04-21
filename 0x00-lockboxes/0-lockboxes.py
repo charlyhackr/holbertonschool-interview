c12a5ebc5351d2776c3ec6944d79be53055d87df
Charlyhackr <930@holbertonschool.com>
2020-04-21 00:14:41 -0500

    see if workk

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    '''function for unlock all'''
+
+    if boxes[0]:
+        return True
+
+    checkr = {0}
+    lista_de_llaves = [0]
+    lon = len(boxes)
+    
+    while lista_de_llaves:
+        caja_espejo = lista_de_llaves.pop()
+        for llave in boxes[caja_espejo]:
+            if isinstance(llave, int) and 0 <= llave < lon and llave not in checkr:
+                checkr.add(llave)
+                lista_de_llaves.add(llave)
+
+    return lon == len(checkr)
