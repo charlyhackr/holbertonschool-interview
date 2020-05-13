751f37d71b569dd5606b87ce616685268414b8b7
Charlyhackr <930@holbertonschool.com>
2020-05-12 23:41:08 -0500

    add files

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#/usr/bin/python3
+import math
+"""
+
+   Solution: boils down to find the prime factors and return their sum
+"""
+
+def sum_prime_factors(n):
+    res = 0
+
+    if n <= 1:
+        return res
+    for i in range(2, int(math.sqrt(n) + 1)):
+        while n % i == 0:
+            n = n // i
+            res += i
+    if n >= 2: # case where n is prime
+        res += n
+    return res
+
+
+def minOperations(n):
+    return sum_prime_factors(n)
