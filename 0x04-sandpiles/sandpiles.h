d65bb2071f00852578928204c483fd2f78149952
Charlyhackr <930@holbertonschool.com>
2020-08-17 22:41:32 -0500

    again compile it

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -3,6 +3,6 @@
 
 #include <stdio.h>
 
-void sandpiles_sum(int grid1[3][31], int grid2[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
-#endif /* SANDPILES_H*/
+#endif /* SANDPILES_H */
