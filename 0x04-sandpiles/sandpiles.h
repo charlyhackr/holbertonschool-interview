16e614dd0a7cc6c1d8cc9b0b1d18be3e08d0ae9d
Charlyhackr <930@holbertonschool.com>
2020-08-17 22:54:38 -0500

    fix all

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,8 +1,14 @@
-#ifndef SANDPILES_H
-#define SANDPILES_H
+#ifndef _SP
+#define _SP
 
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void p(int grid[3][3]);
 
-#endif /* SANDPILES_H */
+#endif /* _SP */
