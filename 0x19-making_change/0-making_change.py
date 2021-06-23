caafc2c73d11461aa7919f89e9e98845c8f4be16
charlyhackr <930@holbertonschool.com>
2021-06-23 21:21:10 +0100

    add all files

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+To search the minimun coins for the total.
+"""
+
+
+def makeChange(coins, total):
+    """
+    Make change using minimum coins given infinite amounts of some
+    denominations and a total to reach.
+    """
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        count += total // coin
+        total = total % coin
+    if total:
+        return -1
+    return count
